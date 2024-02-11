0b1e34db7e5a45d19033d52bc08f81ef2031fa8f
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-02-11 16:08:42 +0530

    feat: build the sdk

---
+++ simple-star-wars-sdk/src/index.js
@@ -0,0 +1,23 @@
+export class SimpleStarWarsSdk {
+  /**
+   * Retrieves all characters from the Star Wars API.
+   * @returns {Promise<Array>} A promise that resolves to an array of character objects.
+   */
+  static async getAllCharacters() {
+    return fetch("https://swapi.dev/api/people")
+      .then((response) => response.json())
+      .then((data) => data.results);
+  }
+
+  /**
+   * Retrieves a Star Wars character by ID.
+   *
+   * @param {number} id - The ID of the character.
+   * @returns {Promise<Object>} A promise that resolves to the character data.
+   */
+  static async getCharacter(id) {
+    return fetch(`https://swapi.dev/api/people/${id}`)
+      .then((response) => response.json())
+      .then((data) => ({})); //! <== update this line to return the data: .then((data) => data)
+  }
+}
